b51f99d5195c7c449d4449f836a2e3b17472e1ee
Vladislav Miheenkov <vladjuha13@gmail.com>
2022-07-11 16:39:43 +0300

    Initial commit

---
+++ src/main/java/org/vlad/springTestApp1/dao/PersonMapper.java
@@ -0,0 +1,22 @@
+package org.vlad.springTestApp1.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.vlad.springTestApp1.models.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+        Person person = new Person();
+
+        person = new Person();
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+
+        return person;
+    }
+}
